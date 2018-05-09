b6d676b6dbc26bdb478692307cddf8e0df603855
KuanYangGH <ykxjtuedu@163.com>
2018-05-09 21:16:33 +0800

    修改前端ipconfig.js

--- src/main/resources/static/config/ipconfig.js
+++ src/main/resources/static/config/ipconfig.js
@@ -2,7 +2,7 @@
 // var ip = "http://202.117.54.42:8080/Yotta"; // 服务器域名(ip) + http端口(9218)+工程名字
 // var ip = "http://yotta.xjtushilei.com:9218/Yotta"; // 服务器域名(ip) + http端口(9218)+工程名字
 //var ip = "http://202.117.54.125:8080/Yotta"; // 服务器域名(ip) + http端口(9218)+工程名字
-var ip = "http://localhost:8080/yotta";
+var ip = "http://202.117.54.42:8080/yotta";
 
 
 // setCookie("name","hayden");
