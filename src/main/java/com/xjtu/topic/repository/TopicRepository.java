2caac8154bce799d12e3978d6aef34e8875dc91c
KuanYangGH <ykxjtuedu@163.com>
2018-11-06 20:20:24 +0800

    优化API:subject/getSubjectTree

--- src/main/java/com/xjtu/topic/repository/TopicRepository.java
+++ src/main/java/com/xjtu/topic/repository/TopicRepository.java
@@ -49,6 +49,19 @@ public interface TopicRepository extends JpaRepository<Topic, Long>, JpaSpecific
     Integer findTopicNumberByDomainId(Long domainId);
 
     /**
+     * 查询所有主题
+     *
+     * @return
+     */
+    @Transactional(rollbackFor = Exception.class)
+    @Query(value = "SELECT * \n" +
+            "FROM topic \n" +
+            "WHERE topic.domain_id IN\n" +
+            "(SELECT domain_id FROM domain);", nativeQuery = true)
+    List<Topic> findAll();
+
+
+    /**
      * 根据课程，查询课程下的主题数
      *
      * @param domainIds
