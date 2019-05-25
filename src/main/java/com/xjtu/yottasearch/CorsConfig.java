8f29b1caf85be00e7f69950098bfeee47ea09be4
script <xjtushilei@foxmail.com>
2019-05-25 22:28:53 +0800

    cors

---
+++ src/main/java/com/xjtu/yottasearch/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.xjtu.yottasearch;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    /**
+     * 允许任何域名使用
+     * 允许任何头
+     * 允许任何方法（post、get等）
+     */
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        // allowCredential 需设置为true
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
