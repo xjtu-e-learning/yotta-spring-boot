9b4d9ad547a91bab521adb7027983ceefdaefbb4
patric-lynn <358812977@163.com>
2020-09-08 17:20:23 +0800

    --story=更新爬虫部分设置

--- src/main/java/com/xjtu/utils/SpiderUtils.java
+++ src/main/java/com/xjtu/utils/SpiderUtils.java
@@ -7,8 +7,10 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,15 +37,19 @@ public class SpiderUtils {
      * @throws Exception
      */
     public static String seleniumWiki(String url) throws Exception {
-		System.setProperty("webdriver.chrome.driver", Config.CHROME_PATH);
-//        System.setProperty("webdriver.ie.driver", Config.IE_PATH);
+        System.setProperty("webdriver.chrome.driver", Config.CHROME_PATH);
+//      System.setProperty("webdriver.ie.driver", Config.IE_PATH);
 //		System.setProperty("phantomjs.binary.path", Config.PHANTOMJS_PATH);
 
-		WebDriver driver = new ChromeDriver();
-//        WebDriver driver = new InternetExplorerDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+//      WebDriver driver = new InternetExplorerDriver();
 //		WebDriver driver = new PhantomJSDriver();
 
         int m = 1;
+        driver.manage().window().setSize(new Dimension(100, 100));
         driver.manage().timeouts().pageLoadTimeout(1000, TimeUnit.SECONDS);
         while (m < 4) {
             try {
@@ -53,8 +59,8 @@ public class SpiderUtils {
                 m++;
                 driver.quit();
 
-//				driver = new ChromeDriver();
-                driver = new InternetExplorerDriver();
+                driver = new ChromeDriver();
+//              driver = new InternetExplorerDriver();
 //				driver = new PhantomJSDriver();
 
                 driver.manage().timeouts().pageLoadTimeout(1000, TimeUnit.SECONDS);
@@ -64,7 +70,7 @@ public class SpiderUtils {
         }
         logger.info("Page title is: " + driver.getTitle());
         String html = driver.getPageSource();
-        Thread.sleep(1000);
+        Thread.sleep(1);
         driver.quit();
         return html;
     }
